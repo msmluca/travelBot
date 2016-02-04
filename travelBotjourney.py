a20d98d89f5ca67dd21c2e83610ca6ffdeea84d3
Luca Minello <luca.minello@moneysupermarket.com>
2016-02-04 17:09:24 +0000

    die Bot can speak

--- travelBotjourney.py
+++ travelBotjourney.py
@@ -1,7 +1,7 @@
 import csv
 import random
 
-class TravelJourney:
+class travelBotjourney:
 
 	def __init__(self):
 		# Read questions
@@ -10,40 +10,66 @@ class TravelJourney:
 		with open(file_destination, 'r') as csvfile:
 		 	questions = csv.reader(csvfile, delimiter=',', quotechar='"')
 		 	next(questions)
-		 	self.keywords = dict()
+		 	self.answer = dict()
 		 	self.questions = dict()
+		 	self.answers_status = dict()
 
 		 	for row in questions:
-		 		self.keywords[row[0]]=None
+		 		self.answer[row[0]]=[]
+
 		 		if row[0] in self.questions:
 		 			self.questions[row[0]].append(row[1])
 		 		else:
 		 			self.questions[row[0]]=[row[1]]
+		 		if row[2] == 'NONE':
+		 			self.answers_status[row[0]] = {'type':row[2],'complete':1}
+		 		else:
+		 			self.answers_status[row[0]] = {'type':row[2],'complete':0}
+		 			
 
 	def set_attribute(self, key, value):
-		if key in self.keywords:
-			self.keywords[key] = value
-		print ("Value %s updated with %d",(key,value))
+		if key in self.answers_status:
+			if self.answers_status[key]['type'] == 'LIST':
+				self.answer[key].append(value)
+			elif self.answers_status[key]['type'] == 'INT':
+				if value.isdigit():
+					self.answer[key] = int(value)
+				else:
+				 return -1
+			else:
+				self.answer[key] = value
+
+		print self.answer
+
+	def complete_answer(self, key, complete):
+		if key in self.answers_status:
+			self.answers_status[key]['complete'] = complete
+
+	def get_answer_type(self, key):
+		if key in self.answers_status:
+			return self.answers_status[key]['type']
+		return None
 
 	def get_attribute(self, key):
-		if key in self.keywords:
-			return self.keywords[key]
+		if key in self.answers_status:
+			return self.answer[key]
 		else:
 			return None
 
 	def next_missing(self):
-		for key, value in self.keywords.items():
-			if value == None:
+		for key, value in self.answers_status.items():
+			if value['complete'] == 0:
+				print("NExt question is",key)
 				return key
 		return None
 
 	def get_question(self, key):
-		if key in self.keywords:
+		if key in self.answers_status:
 			return random.choice(self.questions[key])
 		return None
 
 
 if __name__ == "__main__":
-	a = TravelJourney()
+	a = travelBotjourney()
 	key = a.next_missing()
 	print a.get_question(key)
