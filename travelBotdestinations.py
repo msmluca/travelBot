8ff65b47a8768584a54df5fe049b19dc4ac453a4
Patrick Ho <patrick.ho@moneysupermarket.com>
2016-02-04 15:22:37 +0000

    Fix - Hardcode dictionary creation

--- travelBotdestinations.py
+++ travelBotdestinations.py
@@ -7,22 +7,49 @@ class travelBotdestinations():
 		travelDestinations = {}
 
 		# Create empty activities dictionary
-		f = csv.reader(open(file_destination, 'r'), delimiter=',', quotechar='"')
-		headers = f.next()
-		activities = {activity:0 for activity in headers[1:]}
+		# f = csv.reader(open(file_destination, 'r'), delimiter=',', quotechar='"')
+		# headers = f.next()
+		# activities = {activity:0 for activity in headers[1:]}
 
+		# Create dict of destination activities
+		# with open(file_destination, 'r') as csvfile:
+		# 	destinations = csv.reader(csvfile, delimiter=',', quotechar='"')
+		# 	next(destinations)
+		# 	for row in destinations:
+		# 		for i, x in enumerate(activities.keys()):
+		# 			activities[x] = row[i+1]
+		# 		travelDestinations[row[0] + ', UK'] = activities
+		 
 		# Create dict of destination activities
 		with open(file_destination, 'r') as csvfile:
 			destinations = csv.reader(csvfile, delimiter=',', quotechar='"')
 			next(destinations)
 			for row in destinations:
-				for i, x in enumerate(activities.keys()):
-					activities[x] = row[i+1]
-				travelDestinations[row[0] + ', UK'] = activities
-		 
+				travelDestinations[row[0] + ', UK'] = {
+					'Sights & Landmarks':row[1],
+					'Museums':row[2],
+					'Tours & Activities':row[3],
+					'Nature & Parks':row[4],
+					'Amusement Parks':row[5],
+					'Nightlife':row[6],
+					'Outdoor Activities':row[7],
+					'Shopping':row[8],
+					'Fun & Games':row[9],
+					'Theatre & Concerts':row[10],
+					'Boat Tours & Water Sports':row[11],
+					'Food & Drink':row[12],
+					'Spas & Wellness':row[13],
+					'Casinos & Gambling':row[14],
+					'Transportation':row[15],
+					'Classes & Workshops':row[16],
+					'Traveller Resources':row[17],
+					'Zoos & Aquariums':row[18],
+					'Events':row[19]}
+
 		return travelDestinations
 
 
 if __name__ == "__main__":
 	a = travelBotdestinations()
-	print(a.load_destinations("destinations_test.csv"))
+	b = a.load_destinations("destinations.csv")
+	print(b['Derby, UK'])
